874720dc1a18fccd1b2c8e3b498d58034cb9c203
Surdu Lucian <luciansurdu66@gmail.com>
2023-11-09 15:51:14 +0200

    -

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text>Universal React with Expo</Text>
+      <Text style={{ fontSize: 30 }}>Hello World</Text>
     </View>
   );
 }
