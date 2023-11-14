4b350f39f6d6309c42f434f8236b88f787271889
Pisuc-Sefora-Eunice <sefora.pisuc@stud.ubbcluj.ro>
2023-11-14 12:08:45 +0200

    -

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import AddIngredient from "./frontend/src/components/addIngredient"; // updated import path
 import HomeScreen from "./frontend/src/components/homeScreen";
-import BottomMenu from "./frontend/src/components/bottomMenu";
+// import BottomMenu from "./frontend/src/components/bottomMenu";
 
 //create a stack navigator with bottom menu sticking to the bottom
 const Stack = createStackNavigator();
