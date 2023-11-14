34c66a964b56e96b199104c2e35227648a3634f5
Surdu Lucian <luciansurdu66@gmail.com>
2023-11-14 10:53:05 +0200

    Added home and addIngredient screens + styles

---
+++ frontend/src/components/homeScreen.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AddIngredient from './addIngredient';
+import BottomMenu from './bottomMenu';
+import styles from '../styles/homeScreen.style';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      
+        <View style={styles.overlay}>
+          <Text style={styles.title}>Eat Smart</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate(AddIngredient)}
+          >
+            <Text style={styles.buttonText}>Get Started</Text>
+            </TouchableOpacity>
+        </View>
+      
+    </View>
+  );
+};
+
+
+
+export default HomeScreen;
---
+++ frontend/src/styles/addIngredient.style.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 40,
+        paddingHorizontal: 20
+    },
+    titleText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'black',
+        textAlign: 'center',
+        marginBottom: 20
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#777',
+        padding: 8,
+        margin: 10,
+        width: 200
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: 'black',
+        padding: 10,
+        margin: 10,
+        width: 300
+    },
+    buttonText: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: 'black',
+        textAlign: 'center',
+        marginBottom: 20
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'black',
+        textAlign: 'center',
+        marginBottom: 20
+    }
+    });
+
+    export default styles;
