faac42bd4ef6504122c8c1a543a13aceb40dab88
Surdu Lucian <luciansurdu66@gmail.com>
2023-12-15 01:10:14 +0200

    connected the endpoint for My fridge screen

--- frontend/src/components/gridComponent.js
+++ frontend/src/components/gridComponent.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View, StyleSheet, Dimensions, ScrollView, Text } from 'react-native';
 import AddIngredient from './addIngredient';
+import axios from 'axios';
+import baseURL from "../../assets/constants";
+import { useState, useEffect } from 'react';
 
 const SquareComponent = () => {
   return <View style={styles.square} />;
@@ -9,44 +12,31 @@ const SquareComponent = () => {
 const GridComponent = () => {
   const screenWidth = Dimensions.get('window').width;
   const itemWidth = (screenWidth - 30) / 2; // Subtract padding and margin
-  /* const items = [
-    { id: 1, name: 'Item 1', quantity: 5, expirationDate: '2023-12-31' },
-    { id: 2, name: 'Item 2', quantity: 3, expirationDate: '2023-11-30' },
-    { id: 3, name: 'Item 3', quantity: 2, expirationDate: '2023-12-15' },
-    { id: 4, name: 'Item 4', quantity: 1, expirationDate: '2024-01-15' },
-    // ... 16 more items ...
-    { id: 17, name: 'Item 17', quantity: 3, expirationDate: '2023-12-20' },
-    { id: 18, name: 'Item 18', quantity: 2, expirationDate: '2023-12-25' },
-    { id: 19, name: 'Item 19', quantity: 1, expirationDate: '2024-01-01' },
-    { id: 20, name: 'Item 20', quantity: 4, expirationDate: '2023-12-10' },
-  ]; */
-  
-  const generateItems = () => {
-    const items = [];
-    for (let i = 1; i <= 20; i++) {
-      items.push({
-        id: i,
-        name: `Item ${i}`,
-        quantity: Math.floor(Math.random() * 10) + 1, // Random quantity for demonstration
-        expirationDate: '2023-12-31', // Replace with actual expiration dates
-      });
-    }
-    return items;
-  };
+  const [items, setItems] = useState([]); // Initialize items as an empty array
 
-  const items = generateItems();
+  useEffect(() => {
+    axios.get(baseURL+"/ingredient/")
+      .then((res) => {
+        console.log(res.data.ingredients);
+        setItems(res.data.ingredients);
+      })
+      .catch((error) => {
+        console.error('Failed to fetch items:', error);
+      });
+  }, []);
 
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={styles.gridContainer}>
-            {items.map((item) => (
-                <View style={styles.gridItem} key={item.id}>
-                    <Text style={styles.itemName}>{item.name}</Text>
-                    <Text>Quantity: {item.quantity}</Text>
-                    <Text>Expires: {item.expirationDate}</Text>
-                </View>
-            ))}
+      <View style={styles.gridContainer}>
+        {items.map((item) => (
+          <View style={styles.gridItem} key={item.id}>
+          <Text style={styles.itemName}>{item.name}</Text>
+          <Text>Quantity: {item.quantity}</Text>
+          <Text>Unit: {item.unit}</Text>
+          <Text>Description: {item.description}</Text>
         </View>
+        ))}
+      </View>
     </ScrollView>
   );
 };
