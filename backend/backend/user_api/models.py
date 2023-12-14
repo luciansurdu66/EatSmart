23365212baa26a102dd810173d250d6afa187cb4
Your Name <ruben.radu@stud.ubbcluj.ro>
2023-12-14 17:22:19 +0200

    test

--- backend/backend/user_api/models.py
+++ backend/backend/user_api/models.py
@@ -1,36 +1,45 @@
+# models.py
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 class AppUserManager(BaseUserManager):
-	def create_user(self, email, password=None):
-		if not email:
-			raise ValueError('An email is required.')
-		if not password:
-			raise ValueError('A password is required.')
-		email = self.normalize_email(email)
-		user = self.model(email=email)
-		user.set_password(password)
-		user.save()
-		return user
-	def create_superuser(self, email, password=None):
-		if not email:
-			raise ValueError('An email is required.')
-		if not password:
-			raise ValueError('A password is required.')
-		user = self.create_user(email, password)
-		user.is_superuser = True
-		user.save()
-		return user
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('An email is required.')
+        if not password:
+            raise ValueError('A password is required.')
+        if not username:
+            raise ValueError('An username is required.')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email=email, username=username, password=password, **extra_fields)
 
 class AppUser(AbstractBaseUser, PermissionsMixin):
-	user_id = models.AutoField(primary_key=True)
-	email = models.EmailField(max_length=50, unique=True)
-	username = models.CharField(max_length=50)
-	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['username']
-	objects = AppUserManager()
-	def __str__(self):
-		return self.username
-	
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=50, unique=True)
+    username = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = AppUserManager()
+
+    def __str__(self):
+        return self.username
--- backend/backend/user_api/tests.py
+++ backend/backend/user_api/tests.py
@@ -1,3 +1,54 @@
+# tests.py
 from django.test import TestCase
+from user_api.models import AppUser
 
-# Create your tests here.
+class AppUserTests(TestCase):
+    def setUp(self):
+        self.user_data = {
+            'email': 'test@example.com',
+            'username': 'testuser',
+            'password': 'testpassword',
+        }
+
+    def test_create_user(self):
+        user = AppUser.objects.create_user(email=self.user_data['email'], username=self.user_data['username'], password=self.user_data['password'])
+        self.assertEqual(user.email, self.user_data['email'])
+        self.assertEqual(user.username, self.user_data['username'])
+        self.assertTrue(user.check_password(self.user_data['password']))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        superuser_data = {
+            'email': 'admin@example.com',
+            'username': 'admin',
+            'password': 'adminpassword',
+        }
+        superuser = AppUser.objects.create_superuser(email=superuser_data['email'], username=superuser_data['username'], password=superuser_data['password'])
+        self.assertEqual(superuser.email, superuser_data['email'])
+        self.assertEqual(superuser.username, superuser_data['username'])
+        self.assertTrue(superuser.check_password(superuser_data['password']))
+        self.assertTrue(superuser.is_active)
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_superuser)
+
+    def test_create_user_missing_email(self):
+        with self.assertRaises(ValueError):
+            AppUser.objects.create_user(username='testuser', password='testpassword')
+
+    def test_create_user_missing_password(self):
+        with self.assertRaises(ValueError):
+            AppUser.objects.create_user(email='test@example.com', username='testuser')
+
+    def test_create_superuser_missing_email(self):
+        with self.assertRaises(ValueError):
+            AppUser.objects.create_superuser(username='admin', password='adminpassword')
+
+    def test_create_superuser_missing_password(self):
+        with self.assertRaises(ValueError):
+            AppUser.objects.create_superuser(email='admin@example.com', username='admin')
+
+    def test_str_method(self):
+        user = AppUser.objects.create_user(**self.user_data)
+        self.assertEqual(str(user), self.user_data['username'])
