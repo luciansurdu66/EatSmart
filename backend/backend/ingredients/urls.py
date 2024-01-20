d1738afa231e53493569edbaabb47d10d30eaf3e
Aless-Tiboca <91499890+Aless-Tiboca@users.noreply.github.com>
2024-01-21 00:39:58 +0200

    Delete useless files

--- backend/backend/ingredients/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ingredients/', views.IngredientListCreate.as_view(), name='ingredient-list'),
-    path('ingredients/<int:pk>/', views.IngredientRetrieveUpdateDestroy.as_view(), name='ingredient-detail'),
-]
